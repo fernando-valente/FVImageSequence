0bcd2841870cf285da935b53e949bd8a89ee094d
fernando_valente <fernando@fvalente.org>
2015-06-10 05:01:24 +0100

    Initial release

--- main.m
+++ main.m
@@ -6,12 +6,9 @@
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Cocoa/Cocoa.h>
 
-int main(int argc, char *argv[]) {
-    
-    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-    int retVal = UIApplicationMain(argc, argv, nil, nil);
-    [pool release];
-    return retVal;
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
 }
